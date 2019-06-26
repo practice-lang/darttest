7d3ece7054019c6afc917badd9a9483b3b4e92ad
ttakkku <k2005118@gmail.com>
2019-06-26 19:21:49 +0900

    Add files via upload

---
+++ test.dart
@@ -0,0 +1,17 @@
+void main() {
+  print("hello");
+  Car("ㅁㅇㅁㅇㅁ");
+}
+
+class Car{
+  int seat = 2;
+  int door = 2;
+  String color = "white";
+  String cl = "https://www.youtube.com/watch?v=2ru9TY7AzFs";
+  Car(String msg){
+    /**
+     *  메세지
+     */
+    print(msg);
+  }
+}
